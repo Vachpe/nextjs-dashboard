7eab666a2f8774fb9eb337711a58716f15a324a9
Vachpe <vachpe@yandex.com>
2024-03-26 13:08:33 +0300

    Added speed analytics to main app

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Link from 'next/link';
 import styles from '/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <SpeedInsights/>
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
